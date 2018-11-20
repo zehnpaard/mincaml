91d997ba56a8eb4442c3f78257a9ac2e5a74469b
zehnpaard <zehnpaard@googlemail.com>
2018-11-20 21:48:57 +0900

    Add typing upto occur check

---
+++ typing.ml
@@ -0,0 +1,58 @@
+open Syntax
+
+exception Unify of Type.t * Type.t
+exception Error of t * Type.t * Type.t
+
+let extenv = ref M.empty
+
+let rec deref_type = function
+  | Type.Fun(t1s, t2) -> Type.Fun(List.map deref_typ t1s, deref_typ t2)
+  | Type.Tuple(ts) -> Type.Tuple(List.map deref_typ ts)
+  | Type.Array(t) -> Type.Array(deref_typ t)
+  | Type.Var({ contents = None } as r) ->
+      Format.eprintf "uninstantiated type variable detected; assuming int@.";
+      r := Some(Type.Int);
+      Type.Int
+  | Type.Var({ contents = Some(t) } as r) ->
+      let t' = deref_typ t in
+      r := Some(t');
+      t'
+  | t -> t
+
+let rec deref_id_typ (x, t) -> (x, deref_typ t)
+
+let rec deref_term = function
+  | Not(e) -> Not(deref_term e)
+  | Neg(e) -> Neg(deref_term e)
+  | Add(e1, e2) -> Add(deref_term e1, deref_term e2)
+  | Sub(e1, e2) -> Sub(deref_term e1, deref_term e2)
+  | Eq(e1, e2) -> Eq(deref_term e1, deref_term e2)
+  | LE(e1, e2) -> LE(deref_term e1, deref_term e2)
+  | FNeg(e) -> FNeg(deref_term e)
+  | FAdd(e1, e2) -> FAdd(deref_term e1, deref_term e2)
+  | FSub(e1, e2) -> FSub(deref_term e1, deref_term e2)
+  | FMul(e1, e2) -> FMul(deref_term e1, deref_term e2)
+  | FDiv(e1, e2) -> FDiv(deref_term e1, deref_term e2)
+  | If(e1, e2, e3) -> If(deref_term e1, deref_term e2, deref_term e3)
+  | Let(xt, e1, e2) -> Let(deref_id_typ xt, deref_term e1, deref_term e2)
+  | LetRec({ name = xt; args = yts; body = e1 }, e2) ->
+      LetRec({ name = deref_id_typ xt;
+               args = List.map deref_typ_id yts;
+               body = deref_term e1 },
+             deref_term e2)
+  | App(e, e2) -> App(deref_term e, List.map deref_term es)
+  | Tuple(es) -> Tuple(List.map deref_term es)
+  | LetTuple(xts, e1, e2) -> LetTuple(List.map deref_id_typ xts, deref_term e1, deref_term e2)
+  | Array(e1, e2) -> Array(deref_term e1, deref_term e2)
+  | Get(e1, e2) -> Get(deref_term e1, deref_term e2)
+  | Put(e1, e2, e3) -> Put(deref_term e1, deref_term e2, deref_term e3)
+  | e -> e
+
+let rec occur r1 = function
+  | Type.Fun(t2s, t2) -> List.exists (occur r1) t2s || occur r1 t2
+  | Type.Tuple(t2s) -> List.exists (occur r1) t2s
+  | Type.Array(t2) -> occur r1 t2
+  | Type.Var(r2) when r1 == r2 -> true
+  | Type.Var({ contents = None }) -> false
+  | Type.Var({ contents = Some(t2) }) -> occur r1 t2
+  | _ -> false
