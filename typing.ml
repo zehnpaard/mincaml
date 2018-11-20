8f11ad934345877f2ec736e017e0e3153f87b3ae
zehnpaard <zehnpaard@googlemail.com>
2018-11-20 21:57:58 +0900

    Add unification for type inference

--- typing.ml
+++ typing.ml
@@ -56,3 +56,27 @@ let rec occur r1 = function
   | Type.Var({ contents = None }) -> false
   | Type.Var({ contents = Some(t2) }) -> occur r1 t2
   | _ -> false
+
+let rec unify t1 t2 = match t1, t2 with
+  | Type.Unit, Type.Unit 
+  | Type.Bool, Type.Bool 
+  | Type.Int, Type.Int 
+  | Type.Float, Type.Float -> ()
+  | Type.Fun(t1s, t1'), Type.Fun(t2s, t2') ->
+      (try List.iter2 unify t1s t2s
+       with Invalid_argument("List.iter2") -> raise (Unify(t1, t2)));
+       unify t1' t2'
+  | Type.Tuple(t1s), Type.Tuple(t2s) ->
+      (try List.iter2 unify t1s t2s
+       with Invalid_argument("List.iter2") -> raise (Unify(t1, t2)));
+  | Type.Array(t1), Type.Array(t2) -> unify t1 t2
+  | Type.Var(r1), Type.Var(r2) when r1 == r2 -> ()
+  | Type.Var({ contents = Some(t1') }), _ -> unify t1' t2
+  | _, Type.Var({ contents = Some(t2') }) -> unify t1 t2'
+  | Type.Var({ contents = None } as r1), _ ->
+      if occur r1 t2 then raise (Unify(t1, t2));
+      r1 := Some(t2)
+  | _, Type.Var({ contents = None } as r2)_ ->
+      if occur r2 t1 then raise (Unify(t1, t2));
+      r2 := Some(t1)
+  | _, _ -> raise (Unify(t1, t2))
