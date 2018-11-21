18e91c7289d24741fa2e60774762f591b4752599
zehnpaard <zehnpaard@googlemail.com>
2018-11-21 11:31:13 +0900

    Copy type and implement fv function in kNormal.ml

---
+++ kNormal.ml
@@ -0,0 +1,40 @@
+type t =
+  | Unit
+  | Int of int
+  | Float of float
+  | Neg of Id.t
+  | Add of Id.t * Id.t
+  | Sub of Id.t * Id.t
+  | FNeg of Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t
+  | IfEq of Id.t * Id.t * t * t
+  | IfLE of Id.t * Id.t * t * t
+  | Let of (Id.t * Type.t) * t  * t
+  | Var of Id.t
+  | LetRec of fundef * t
+  | App of Id.t * Id.t list
+  | Tuple of Id.t list
+  | LetTuple of (Id.t * Type.t) list * Id.t * t
+  | Get of Id.t * Id.t
+  | Put of Id.t * Id.t * Id.t
+  | ExtArray of Id.t
+  | ExtFunApp of Id.t * Id.t list
+and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
+
+let rec fv = function
+  | Unit | Int(_) | Float(_) | ExtArray(_) -> S.empty
+  | Neg(x) | FNeg(x) -> S.singleton x
+  | Add(x, y) | Sub(x, y) | FAdd(x, y) | FSub(x, y) | FMul(x, y) | FDiv(x, y) | Get(x, y) -> S.of_list [x; y]
+  | IfEq(x, y, e1, e2) | IfLE(x, y, e1, e2) -> S.add x (S.add y (S.union (fv e1) (fv e2)))
+  | Let((x, t), e1, e2) -> S.union (fv e1) (S.remove x (fv e2))
+  | Var(x) -> S.singleton x
+  | LetRec({ name = (x, t); args = yts; body = e1 }, e2) ->
+      let zs = S.diff (fv e1) (S.of_list (List.map fst yts)) in
+      S.diff (S.union zs (fv e2)) (S.singleton x)
+  | App(x, ys) -> S.of_list (x::ys)
+  | Tuple(xs) | ExtFunApp(_, xs) -> S.of_list xs
+  | Put(x, y, z) -> S.of_list [x; y; z]
+  | LetTuple(xs, y, e) -> S.add y (S.diff (fv e) (S.of_list (List.map fst xs)))
