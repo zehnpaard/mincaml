173f417d5cae42b61711821576149afc29cfb0ce
zehnpaard <zehnpaard@googlemail.com>
2018-11-23 20:47:47 +0900

    Implement Closure.t type and free variable function

---
+++ closure.ml
@@ -0,0 +1,58 @@
+type closure = { entry : Id.l; actual_fv : Id.t list }
+type t =
+  | Unit
+  | Int of int
+  | Float of float
+  | Neg of Id.t
+  | Add of Id.t * Id.t
+  | Sub of Id.t * Id.t
+  | FNeg of Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t
+  | IfEq of Id.t * Id.t * t * t
+  | IfLE of Id.t * Id.t * t * t
+  | Let of (Id.t * Type.t) * t * t
+  | Var of Id.t
+  | MakeCls of (Id.t * Type.t) * closure * t
+  | AppCls of Id.t * Id.t list
+  | AppDir of Id.l * Id.t list
+  | Tuple of Id.t list
+  | LetTuple of (Id.t * Type.t) list * Id.t * t
+  | Get of Id.t * Id.t
+  | Put of Id.t * Id.t * Id.t
+  | ExtArray of Id.l
+type fundef = { name : Id.l * Type.t;
+                args : (Id.t * Type.t) list;
+                formal_fv : (Id.t * Type.t) list;
+                body : t }
+type prog = Prog of fundef list * t
+
+let rec fv = function
+  | Unit
+  | Int(_)
+  | Float(_)
+  | ExtArray(_) -> S.empty
+  | Neg(x)
+  | FNeg(x) -> S.singleton x
+  | Add(x, y)
+  | Sub(x, y)
+  | FAdd(x, y)
+  | FSub(x, y)
+  | FMul(x, y)
+  | FDiv(x, y)
+  | Get(x, y) -> S.of_list [x; y]
+  | IfEq(x, y, e1, e2)
+  | IfLE(x, y, e1, e2) -> S.add x (S.add y (S.union (fv e1) (fv e2)))
+  | Let((x, t), e1, e2) -> S.union (fv e1) (S.remove x (fv e2))
+  | Var(x) -> S.singleton x
+  | MakeCls ((x, t), { entry = l; actual_fv = ys }, e) ->
+      S.remove x (S.union (S.of_list ys) (fv e))
+  | AppCls(x, ys) -> S.of_list (x :: ys)
+  | AppDir(_, xs)
+  | Tuple(xs) -> S.of_list xs
+  | LetTuple(xts, y, e) -> S.add y (S.diff (fv e) (S.of_list (List.map fst xts)))
+  | Put(x, y, z) -> S.of_list [x; y; z]
+
+let toplevel : fundef list ref = ref []
